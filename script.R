a51e07eee4ef7a03308238d610d0c9f5c0f92db5
Tenzin Gyaltsen <tenzingyaltsen16@gmail.com>
2025-01-30 18:45:31 -0500

    Narrowed down parameter changes to epoch number, batch size and drop out rate. Script complete.

--- script.R
+++ script.R
@@ -37,13 +37,14 @@ network0 <- keras_model_sequential() %>%
 network0
 
 # Compile the base neural network.
-network0 %>% compile(optimizer = "rmsprop", 
+network0 %>% compile(optimizer = "sgd", 
                      loss = "categorical_crossentropy", 
                      metrics = c("accuracy", "mse"))
 
 # Train the base neural network and save into history object.
-hist0 <- network0 %>% fit(train_images, train_labels, epochs = 5,
-                          batch_size = 128)
+hist0 <- network0 %>% fit(train_images, train_labels, epochs = 10,
+                          batch_size = 128, view_metrics=T, 
+                          verbose = 1, validation_split=0.2)
 plot(hist0)
 
 # Assess the base network using the test data.
@@ -51,20 +52,37 @@ results0 <- network0 %>% evaluate(test_images, test_labels)
 results0
 
 #### Create networks with increased epoch numbers. ####
-# Define, compile and assess the network.
-network_epoch <- keras_model_sequential() %>%
+# Define, compile and assess the networks.
+# 15 epochs.
+network_epoch1 <- keras_model_sequential() %>%
   layer_dense(units = 512, activation = "relu", 
               input_shape = (28 * 28)) %>%
   layer_dense(units = 10, activation = "softmax")
-network_epoch %>% compile(optimizer = "rmsprop", 
+network_epoch1 %>% compile(optimizer = "sgd", 
                      loss = "categorical_crossentropy", 
                      metrics = c("accuracy", "mse"))
-hist_epoch1 <- network_epoch %>% fit(train_images, train_labels, 
-                                     epochs = 15, batch_size = 128)
-hist_epoch2 <- network_epoch %>% fit(train_images, train_labels, 
-                                     epochs = 20, batch_size = 128)
-results_epoch <- network_epoch %>% evaluate(test_images, test_labels)
-results_epoch
+hist_epoch1 <- network_epoch1 %>% fit(train_images, train_labels, 
+                                     epochs = 15, batch_size = 128, view_metrics=T, 
+                                     verbose = 1, validation_split=0.2)
+results_epoch1 <- network_epoch1 %>% evaluate(test_images, test_labels)
+
+# 20 epochs.
+network_epoch2 <- keras_model_sequential() %>%
+  layer_dense(units = 512, activation = "relu", 
+              input_shape = (28 * 28)) %>%
+  layer_dense(units = 10, activation = "softmax")
+network_epoch2 %>% compile(optimizer = "sgd", 
+                          loss = "categorical_crossentropy", 
+                          metrics = c("accuracy", "mse"))
+hist_epoch2 <- network_epoch2 %>% fit(train_images, train_labels, 
+                                     epochs = 20, batch_size = 128, view_metrics=T, 
+                                     verbose = 1, validation_split=0.2)
+results_epoch2 <- network_epoch2 %>% evaluate(test_images, test_labels)
+
+# Compare.
+results0
+results_epoch1
+results_epoch2
 hist0
 hist_epoch1
 hist_epoch2
@@ -73,53 +91,75 @@ plot(hist_epoch1)
 plot(hist_epoch2)
 
 #### Create networks with larger batch sizes. ####
-network_bs <- keras_model_sequential() %>%
+# Batch size of 28 * 2. 
+network_bs1 <- keras_model_sequential() %>%
   layer_dense(units = 512, activation = "relu", 
               input_shape = (28 * 28)) %>%
   layer_dense(units = 10, activation = "softmax")
-network_bs
-network_bs %>% compile(optimizer = "rmsprop", 
+network_bs1 %>% compile(optimizer = "sgd", 
                      loss = "categorical_crossentropy", 
                      metrics = c("accuracy", "mse"))
-hist_bs1 <- network_bs %>% fit(train_images, train_labels, epochs = 5,
-                          batch_size = 128 * 2)
-hist_bs2 <- network_bs %>% fit(train_images, train_labels, epochs = 5,
-                              batch_size = 128 * 3)
-results_bs <- network_bs %>% evaluate(test_images, test_labels)
-results_bs
+hist_bs1 <- network_bs1 %>% fit(train_images, train_labels, epochs = 5,
+                          batch_size = 128 * 2, view_metrics = T, 
+                          verbose = 1, validation_split = 0.2)
+results_bs1 <- network_bs1 %>% evaluate(test_images, test_labels)
+
+# Batch size of 28 * 3. 
+network_bs2 <- keras_model_sequential() %>%
+  layer_dense(units = 512, activation = "relu", 
+              input_shape = (28 * 28)) %>%
+  layer_dense(units = 10, activation = "softmax")
+network_bs2 %>% compile(optimizer = "sgd", 
+                        loss = "categorical_crossentropy", 
+                        metrics = c("accuracy", "mse"))
+hist_bs2 <- network_bs2 %>% fit(train_images, train_labels, epochs = 5,
+                                batch_size = 128 * 3, view_metrics = T, 
+                                verbose = 1, validation_split = 0.2)
+results_bs2 <- network_bs2 %>% evaluate(test_images, test_labels)
+
+# Compare.
+results0
+results_bs1
+results_bs2
 hist0
 hist_bs1
-hist_bs
+hist_bs2
 plot(hist0)
 plot(hist_bs1)
 plot(hist_bs2)
 
 #### Create networks with drop out. ####
+# Drop out rate of 0.3.
 network_do1 <- keras_model_sequential() %>%
   layer_dense(units = 512, activation = "relu", 
               input_shape = (28 * 28)) %>%
   layer_dropout(rate = 0.3) %>%
   layer_dense(units = 10, activation = "softmax")
-network_do1
-network_do1 %>% compile(optimizer = "rmsprop", 
+network_do1 %>% compile(optimizer = "sgd", 
                      loss = "categorical_crossentropy", 
                      metrics = c("accuracy", "mse"))
+hist_do1 <- network_do1 %>% fit(train_images, train_labels, epochs = 5,
+                                batch_size = 128, view_metrics=T, 
+                                verbose = 1, validation_split=0.2)
+results_do1 <- network_do1 %>% evaluate(test_images, test_labels)
+
+# Drop out rate of 0.8.
 network_do2 <- keras_model_sequential() %>%
   layer_dense(units = 512, activation = "relu", 
               input_shape = (28 * 28)) %>%
   layer_dropout(rate = 0.8) %>%
   layer_dense(units = 10, activation = "softmax")
-network_do2
-network_do2 %>% compile(optimizer = "rmsprop", 
+network_do2 %>% compile(optimizer = "sgd", 
                        loss = "categorical_crossentropy", 
                        metrics = c("accuracy", "mse"))
-hist_do1 <- network_do1 %>% fit(train_images, train_labels, epochs = 5,
-                          batch_size = 128)
 hist_do2 <- network_do2 %>% fit(train_images, train_labels, epochs = 5,
-                             batch_size = 128)
-results_do1 <- network_do1 %>% evaluate(test_images, test_labels)
-results_do1
+                             batch_size = 128, view_metrics=T, 
+                             verbose = 1, validation_split=0.2)
 results_do2 <- network_do2 %>% evaluate(test_images, test_labels)
+
+# Compare.
+results0
+results_do1
 results_do2
 hist0
 hist_do1
@@ -127,34 +167,3 @@ hist_do2
 plot(hist0)
 plot(hist_do1)
 plot(hist_do2)
-
-#### Extra. ####
-# Create network with increased learning rate.
-network4 <- keras_model_sequential() %>%
-  layer_dense(units = 512, activation = "relu", 
-              input_shape = (28 * 28)) %>%
-  layer_dropout(rate = 0.8) %>%
-  layer_dense(units = 10, activation = "softmax")
-network4
-network4 %>% compile(optimizer = optimizer_rmsprop(learning_rate = 0.3), 
-                     loss = "categorical_crossentropy", 
-                     metrics = c("accuracy", "mse"))
-hist4 <- network4 %>% fit(train_images, train_labels, epochs = 5,
-                          batch_size = 128)
-results4 <- network4 %>% evaluate(test_images, test_labels)
-results4
-
-# Create network with increased momentum.
-network5 <- keras_model_sequential() %>%
-  layer_dense(units = 512, activation = "relu", 
-              input_shape = (28 * 28)) %>%
-  layer_dropout(rate = 0.8) %>%
-  layer_dense(units = 10, activation = "softmax")
-network5
-network5 %>% compile(optimizer = optimizer_rmsprop(momentum = 0.3), 
-                     loss = "categorical_crossentropy", 
-                     metrics = c("accuracy", "mse"))
-hist5 <- network5 %>% fit(train_images, train_labels, epochs = 5,
-                          batch_size = 128)
-results5 <- network5 %>% evaluate(test_images, test_labels)
-results5
