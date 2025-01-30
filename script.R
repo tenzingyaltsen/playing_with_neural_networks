41bfe3f0ee0ea55b0f2c51857a41fb4c28240afb
Tenzin Gyaltsen <tenzingyaltsen16@gmail.com>
2025-01-30 01:41:16 -0500

    Initial commit.

---
+++ script.R
@@ -0,0 +1,42 @@
+# Coding Exercise 1
+
+#### Setting Up ####
+# Load libraries.
+library(keras)
+library(ggplot2)
+
+# Build simple neural network.
+mnist <- dataset_mnist()
+train_images <- mnist$train$x
+train_labels <- mnist$train$y
+test_images <- mnist$test$x
+test_labels <- mnist$test$y
+
+# Pre-processing images, flatten image arrays to vectors and scale.
+train_images <- array_reshape(train_images, c(60000, 28 * 28))
+train_images <- train_images / 255
+test_images <- array_reshape(test_images, c(10000, 28 * 28))
+test_images <- test_images / 255
+
+# Pre-processing labels, one hot encoding.
+train_labels <- to_categorical(train_labels)
+test_labels <- to_categorical(test_labels)
+
+# Shuffle the training data.
+set.seed(123)
+I <- sample.int(nrow(train_images))
+train_images <- train_images[I,]
+train_labels <- train_labels[I,]
+
+# Use sequential approach to define simple (base) neural network.
+network0 <- keras_model_sequential() %>%
+  layer_dense(units = 512, activation = "relu", 
+              input_shape = (28 * 28)) %>%
+  layer_dense(units = 10, activation = "softmax")
+network0
+
+# Compile the base neural network.
+network0 %>% compile(optimizer = "rmsprop", 
+                     loss = "categorical_crossentropy", 
+                     metrics = c("accuracy", "mse"))
+
