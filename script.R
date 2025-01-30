3322207df0e7a9ab43cd339b65d6514b70dc72d4
Tenzin Gyaltsen <tenzingyaltsen16@gmail.com>
2025-01-30 14:51:43 -0500

    Narrowed down parameter changes to epoch number, batch size and drop out rate. Included 2 values for each parameter change.

--- script.R
+++ script.R
@@ -50,31 +50,111 @@ plot(hist0)
 results0 <- network0 %>% evaluate(test_images, test_labels)
 results0
 
-#### Create network with different epoch number. ####
+#### Create networks with increased epoch numbers. ####
 # Define, compile and assess the network.
-network1 <- keras_model_sequential() %>%
+network_epoch <- keras_model_sequential() %>%
   layer_dense(units = 512, activation = "relu", 
               input_shape = (28 * 28)) %>%
   layer_dense(units = 10, activation = "softmax")
-network1
-network1 %>% compile(optimizer = "rmsprop", 
+network_epoch %>% compile(optimizer = "rmsprop", 
                      loss = "categorical_crossentropy", 
                      metrics = c("accuracy", "mse"))
-hist1 <- network1 %>% fit(train_images, train_labels, epochs = 15,
-                          batch_size = 128)
-results1 <- network1 %>% evaluate(test_images, test_labels)
-results1
+hist_epoch1 <- network_epoch %>% fit(train_images, train_labels, 
+                                     epochs = 15, batch_size = 128)
+hist_epoch2 <- network_epoch %>% fit(train_images, train_labels, 
+                                     epochs = 20, batch_size = 128)
+results_epoch <- network_epoch %>% evaluate(test_images, test_labels)
+results_epoch
+hist0
+hist_epoch1
+hist_epoch2
+plot(hist0)
+plot(hist_epoch1)
+plot(hist_epoch2)
 
-#### Create network with larger batch size. ####
-network2 <- keras_model_sequential() %>%
+#### Create networks with larger batch sizes. ####
+network_bs <- keras_model_sequential() %>%
   layer_dense(units = 512, activation = "relu", 
               input_shape = (28 * 28)) %>%
   layer_dense(units = 10, activation = "softmax")
-network2
-network2 %>% compile(optimizer = "rmsprop", 
+network_bs
+network_bs %>% compile(optimizer = "rmsprop", 
                      loss = "categorical_crossentropy", 
                      metrics = c("accuracy", "mse"))
-hist2 <- network2 %>% fit(train_images, train_labels, epochs = 5,
-                          batch_size = 384)
-results2 <- network2 %>% evaluate(test_images, test_labels)
-results2
+hist_bs1 <- network_bs %>% fit(train_images, train_labels, epochs = 5,
+                          batch_size = 128 * 2)
+hist_bs2 <- network_bs %>% fit(train_images, train_labels, epochs = 5,
+                              batch_size = 128 * 3)
+results_bs <- network_bs %>% evaluate(test_images, test_labels)
+results_bs
+hist0
+hist_bs1
+hist_bs
+plot(hist0)
+plot(hist_bs1)
+plot(hist_bs2)
+
+#### Create networks with drop out. ####
+network_do1 <- keras_model_sequential() %>%
+  layer_dense(units = 512, activation = "relu", 
+              input_shape = (28 * 28)) %>%
+  layer_dropout(rate = 0.3) %>%
+  layer_dense(units = 10, activation = "softmax")
+network_do1
+network_do1 %>% compile(optimizer = "rmsprop", 
+                     loss = "categorical_crossentropy", 
+                     metrics = c("accuracy", "mse"))
+network_do2 <- keras_model_sequential() %>%
+  layer_dense(units = 512, activation = "relu", 
+              input_shape = (28 * 28)) %>%
+  layer_dropout(rate = 0.8) %>%
+  layer_dense(units = 10, activation = "softmax")
+network_do2
+network_do2 %>% compile(optimizer = "rmsprop", 
+                       loss = "categorical_crossentropy", 
+                       metrics = c("accuracy", "mse"))
+hist_do1 <- network_do1 %>% fit(train_images, train_labels, epochs = 5,
+                          batch_size = 128)
+hist_do2 <- network_do2 %>% fit(train_images, train_labels, epochs = 5,
+                             batch_size = 128)
+results_do1 <- network_do1 %>% evaluate(test_images, test_labels)
+results_do1
+results_do2 <- network_do2 %>% evaluate(test_images, test_labels)
+results_do2
+hist0
+hist_do1
+hist_do2
+plot(hist0)
+plot(hist_do1)
+plot(hist_do2)
+
+#### Extra. ####
+# Create network with increased learning rate.
+network4 <- keras_model_sequential() %>%
+  layer_dense(units = 512, activation = "relu", 
+              input_shape = (28 * 28)) %>%
+  layer_dropout(rate = 0.8) %>%
+  layer_dense(units = 10, activation = "softmax")
+network4
+network4 %>% compile(optimizer = optimizer_rmsprop(learning_rate = 0.3), 
+                     loss = "categorical_crossentropy", 
+                     metrics = c("accuracy", "mse"))
+hist4 <- network4 %>% fit(train_images, train_labels, epochs = 5,
+                          batch_size = 128)
+results4 <- network4 %>% evaluate(test_images, test_labels)
+results4
+
+# Create network with increased momentum.
+network5 <- keras_model_sequential() %>%
+  layer_dense(units = 512, activation = "relu", 
+              input_shape = (28 * 28)) %>%
+  layer_dropout(rate = 0.8) %>%
+  layer_dense(units = 10, activation = "softmax")
+network5
+network5 %>% compile(optimizer = optimizer_rmsprop(momentum = 0.3), 
+                     loss = "categorical_crossentropy", 
+                     metrics = c("accuracy", "mse"))
+hist5 <- network5 %>% fit(train_images, train_labels, epochs = 5,
+                          batch_size = 128)
+results5 <- network5 %>% evaluate(test_images, test_labels)
+results5
